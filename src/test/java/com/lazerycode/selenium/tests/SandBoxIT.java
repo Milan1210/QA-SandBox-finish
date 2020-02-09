919a106fec49e5db23711d77029b7425c497bae8
milan1210 <milan99191@live.com>
2020-02-09 12:04:41 +0100

    two_changes

--- src/test/java/com/lazerycode/selenium/tests/SandBoxIT.java
+++ src/test/java/com/lazerycode/selenium/tests/SandBoxIT.java
@@ -14,6 +14,7 @@ import org.testng.annotations.Test;
 
 public class SandBoxIT extends DriverBase {
     //Test one branch
+    //Two test brach
     private WebDriver driver;
     private String siteUrl = System.getProperty("site.url");
     private WebDriverWait wait;
